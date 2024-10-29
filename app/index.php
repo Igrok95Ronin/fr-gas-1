b4ecb1baff32aaec22dbad880851ad0c5845ae6c
Rizvan <xl92@mail.ru>
2024-10-29 11:21:18 +0300

    add: finish site

--- app/index.php
+++ app/index.php
@@ -1,11 +1,11 @@
 <?php
-$site_data      = json_decode(file_get_contents('http://templates.jquery.link/api/' . $_SERVER['HTTP_HOST']), true);
+$site_data      = json_decode(file_get_contents('http://local.jquery.link/api/' . $_SERVER['HTTP_HOST']), true);
 
 $phone_name     = $site_data['phone_name'];
 $phone_href     = $site_data['phone_href'];
 
-$text           = str_replace('+', ' ', trim($_GET['t'] ?? 'Entrumpelung'));
-$city           = str_replace('+', ' ', trim($_GET['n'] ?? 'in der nahe'));
+$text           = str_replace('+', ' ', trim($_GET['t'] ?? 'Сhauffagiste à'));
+$city           = str_replace('+', ' ', trim($_GET['n'] ?? ''));
 
 $title = $text . ' ' . $city;
 ?>
@@ -32,21 +32,21 @@ $title = $text . ' ' . $city;
                 <div class="col-12 containerP0">
                     <div class="header__box">
                         <div class="header__left">
-                            <h1 class="header__title">Сhauffagiste à +proximité+</h1>
+                            <h1 class="header__title"><?= $city == "" ? "Сhauffagiste à proximité" : $title ?></h1>
                             <div class="header__imgWrapper">
                                 <img class="header__img" src="./assets/img/mainFon.jpg" alt="Сhauffagiste à proximité">
                             </div>
                         </div>
                         <div class="header__right">
                             <p class="header__txt">
-                                Vous cherchez un <strong>chauffagiste à +proximité+</strong> ? Nos experts sont <strong> disponibles 24h/24 pour vos réparations</strong>. Intervention rapide en 1 heure garantie. Profitez de notre service fiable avec assurance sur toutes nos prestations. Contactez-nous dès maintenant !
+                                Vous cherchez un <strong>chauffagiste à <?= $city == "" ? "proximité" : $city ?> </strong> ? Nos experts sont <strong> disponibles 24h/24 pour vos réparations</strong>. Intervention rapide en 1 heure garantie. Profitez de notre service fiable avec assurance sur toutes nos prestations. Contactez-nous dès maintenant !
                             </p>
                             <div class="header__wrapperMiniImg">
                                 <img class="header__imgMini" src="./assets/img/gramata.png" alt="gramata">
                                 <img class="header__imgMini" src="./assets/img/stars.png" alt="stars">
                             </div>
                             <div class="header__tellWrapper">
-                                <a class="header__tell" href="tel:111222333"><span>111222333</span></a>
+                                <a class="header__tell" href=<?= $phone_href ?>><span><?= $phone_name ?></span></a>
                             </div>
                         </div>
                     </div>
@@ -65,12 +65,12 @@ $title = $text . ' ' . $city;
                                 <div class="boilerRepair__imgWrapper">
                                     <img class="boilerRepair__img" src="./assets/img/mainFon2.jpg" alt="mainFon2">
                                 </div>
-                                <h2 class="boilerRepair__subTitle">Réparation de chaudières à +proximité+</h2>
+                                <h2 class="boilerRepair__subTitle">Réparation de chaudières à <?= $city == "" ? "proximité" : $city ?></h2>
                             </div>
                             <div class="boilerRepair__right">
-                                <p class="boilerRepair__txt">Vous rencontrez des problèmes avec votre chaudière ? Nous proposons des <strong> services de réparation rapides et efficaces à +proximité+</strong>. Nos techniciens qualifiés sont disponibles pour résoudre toute panne, qu'il s'agisse d'une fuite d'eau ou d'autres dysfonctionnements.</p>
+                                <p class="boilerRepair__txt">Vous rencontrez des problèmes avec votre chaudière ? Nous proposons des <strong> services de réparation rapides et efficaces à <?= $city == "" ? "proximité" : $city ?></strong>. Nos techniciens qualifiés sont disponibles pour résoudre toute panne, qu'il s'agisse d'une fuite d'eau ou d'autres dysfonctionnements.</p>
                                 <div class="boilerRepair__wrapperBtn">
-                                    <a class="boilerRepair__tell" href="tel:111222333"><span>111222333</span></a>
+                                    <a class="boilerRepair__tell" href=<?= $phone_href ?>><span><?= $phone_name ?></span></a>
                                     <a class="boilerRepair__message" href="#anchorForm"><span>Envoyer un message</span></a>
                                 </div>
                             </div>
@@ -127,10 +127,10 @@ $title = $text . ' ' . $city;
                                 <div class="boilerRepair__imgWrapper">
                                     <img class="boilerRepair__img" src="./assets/img/mainFon3.jpg" alt="mainFon3">
                                 </div>
-                                <h2 class="boilerRepair__subTitle">Installation de chaudières ++ </h2>
+                                <h2 class="boilerRepair__subTitle">Installation de chaudières <?= $city == "" ? "" : $city ?> </h2>
                             </div>
                             <div class="boilerRepair__right">
-                                <p class="boilerRepair__txt">Nous offrons un service <strong> d'installation de chaudières à gaz de haute qualité à +proximité+</strong>. Notre équipe professionnelle garantit une installation conforme aux normes de sécurité. Contactez-nous pour une consultation gratuite ! </p>
+                                <p class="boilerRepair__txt">Nous offrons un service <strong> d'installation de chaudières à gaz de haute qualité à <?= $city == "" ? "proximité" : $city ?></strong>. Notre équipe professionnelle garantit une installation conforme aux normes de sécurité. Contactez-nous pour une consultation gratuite ! </p>
                                 <p class="boilerRepair__txt">- <strong> Installation de chaudières à gaz </strong>: Installation conforme aux normes de sécurité.
                                 <p class="boilerRepair__txt">- <strong> Consultation gratuite </strong>: Évaluation de vos besoins en chauffage sans engagement.</p>
                                 <p class="boilerRepair__txt">- <strong> Entretien et maintenance </strong>: Services réguliers pour garantir le bon fonctionnement de votre chaudière.</p>
@@ -149,10 +149,10 @@ $title = $text . ' ' . $city;
                                 <div class="boilerRepair__imgWrapper">
                                     <img class="boilerRepair__img" src="./assets/img/mainFon4.png" alt="mainFon4">
                                 </div>
-                                <h2 class="boilerRepair__subTitle">Entretien de chaudières 24/7 ++</h2>
+                                <h2 class="boilerRepair__subTitle">Entretien de chaudières 24/7 <?= $city == "" ? "" : $city ?></h2>
                             </div>
                             <div class="boilerRepair__right">
-                                <p class="boilerRepair__txt">L'entretien régulier de votre chaudière est essentiel pour éviter des pannes coûteuses. Nous sommes <strong> disponibles 24 heures sur 24, 7 jours sur 7</strong>, pour assurer le bon fonctionnement de votre équipement à +proximité+. Un entretien préventif permet de prolonger la durée de vie de votre chaudière.</p>
+                                <p class="boilerRepair__txt">L'entretien régulier de votre chaudière est essentiel pour éviter des pannes coûteuses. Nous sommes <strong> disponibles 24 heures sur 24, 7 jours sur 7</strong>, pour assurer le bon fonctionnement de votre équipement à <?= $city == "" ? "proximité" : $city ?>. Un entretien préventif permet de prolonger la durée de vie de votre chaudière.</p>
                                 <div class="boilerRepair__wrapperBtn boilerRepair__center">
                                     <a class="boilerRepair__message" href="#anchorForm"><span>Envoyer un message</span></a>
                                 </div>
@@ -171,12 +171,12 @@ $title = $text . ' ' . $city;
                                 <div class="boilerRepair__imgWrapper">
                                     <img class="boilerRepair__img" src="./assets/img/mainFon5.png" alt="mainFon5">
                                 </div>
-                                <h2 class="boilerRepair__subTitle">Intervention rapide	++</h2>
+                                <h2 class="boilerRepair__subTitle">Intervention rapide <?= $city == "" ? "" : $city ?></h2>
                             </div>
                             <div class="boilerRepair__right">
-                                <p class="boilerRepair__txt">Nous comprenons l'importance d'une intervention rapide. Nos techniciens arrivent dans un délai d'1 à 2 heures pour résoudre vos <strong> problèmes de chaudière à +proximité+</strong>. Faites- nous confiance pour un service rapide et fiable !</p>
+                                <p class="boilerRepair__txt">Nous comprenons l'importance d'une intervention rapide. Nos techniciens arrivent dans un délai d'1 à 2 heures pour résoudre vos <strong> problèmes de chaudière à <?= $city == "" ? "proximité" : $city ?></strong>. Faites- nous confiance pour un service rapide et fiable !</p>
                                 <div class="boilerRepair__wrapperBtn boilerRepair__center">
-                                    <a class="boilerRepair__tell" href="tel:111222333"><span>111222333</span></a>
+                                    <a class="boilerRepair__tell" href=<?= $phone_href ?>><span><?= $phone_name ?></span></a>
                                 </div>
                             </div>
                         </div>
@@ -235,7 +235,7 @@ $title = $text . ' ' . $city;
         </section>
         <section class='btnFixed'>
             <div class='btnFixed__box'>
-                <a class="btnFixed__btn" href="<?= $phone_href ?>" ><span>111222333</span></a>
+                <a class="btnFixed__btn" href="<?= $phone_href ?>" ><span><?= $phone_name ?></span></a>
             </div>
         </section>
     </main>
